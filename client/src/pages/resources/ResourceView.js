ea7bbf23315e98020115bc87b6e4c868e5e41931
Leif Swenson <leifswenson@gmail.com>
2018-04-20 21:33:34 +0000

    added react-router

---
+++ client/src/pages/resources/ResourceView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ResourceCard from '../../components/ResourceCard';
+import Section from '../../components/Section';
+
+
+const ResourceView = () => (
+  <Section>
+    <ResourceCard />
+  </Section>
+);
+
+export default ResourceView;
