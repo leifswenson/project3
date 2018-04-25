9afa6e3019c7858ee582b505ca7b3c0d01a5561b
Leif Swenson <leifswenson@gmail.com>
2018-04-25 16:23:21 +0000

    added links to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 
 const Navbar = props => (
   <nav class="navbar navbar-light bg-light">
-    <span class="navbar-brand mb-0 h1">School Scource</span>
+    <Link className="navbar-brand" to="/">
+      School Scource
+    </Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/resources/new" className="nav-link">
+            New
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/resources" className="nav-link">
+            Search
+          </Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
