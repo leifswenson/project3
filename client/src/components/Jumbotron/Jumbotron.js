61364a5132e07ddb9a87b83edf30d8503e57907a
Kadiro Elemo <you@example.com>
2018-04-28 12:49:44 -0500

    edited styling

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -5,8 +5,8 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
   <div className="jumbotron jumbotron-fluid chunk">
     <div className="container">
-      <h1 className="display-4 text-center">School Source</h1>
-      <p className="lead text-center">If your school has stuff that they dont need, maybe another school could use that stuff.</p>
+      <h1 className="display-4 text-center cursive">School Source</h1>
+      <p className="lead text-center">If your school has stuff that they don't need, maybe another school could use that stuff.</p>
     </div>
   </div>
 );
