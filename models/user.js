513c4245be2e9b3aafed1747d637b203b24f210c
Leif Swenson <leifswenson@gmail.com>
2018-04-30 20:06:42 +0000

    added user routes, controllers and api

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true},
+  name: { type: String, required: true },
+  school: { type: String, required: true },
+});
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
