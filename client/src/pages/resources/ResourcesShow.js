2cefac3f5452b8e9de125e882dfb3c30ee327fcb
Leif Swenson <leifswenson@er-10-129-73-170.wireless.umn.edu>
2018-05-05 13:17:27 -0500

    form redirect

--- client/src/pages/resources/ResourcesShow.js
+++ client/src/pages/resources/ResourcesShow.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
 import Section from '../../components/Section';
 import API from "../../utils/API";
 
