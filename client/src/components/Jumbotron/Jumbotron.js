cfafe11add1ad7cf9de90195d0707c9d939abc55
Lockn008 <31474251+Lockn008@users.noreply.github.com>
2018-05-07 19:54:58 -0500

    Update Jumbotron.js

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -6,7 +6,7 @@ const Jumbotron = () => (
   <div className="jumbotron jumbotron-fluid chunk">
     <div className="container">
       <h1 className="display-4 text-center cursive">School Source</h1>
-      <h5 className="display-4 text-center">Making use of schools resources in efficient and effective ways. </h5>
+      <h4 className="display-4 text-center">Making use of schools resources in efficient and effective ways. </h4>
       <p className="lead text-center">School Source allows schools to share, borrow, or transfer resources between schools.  If you are affiliated with a school, simply login to browse equipment, textbooks, or anything else that would help improve education at your institution.</p>
     </div>
   </div>
